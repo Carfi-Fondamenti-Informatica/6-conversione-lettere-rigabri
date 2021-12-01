b57b42aa4eb9cbbc71cddb2797c5792cef1b2418
Gabriele Rinaldis <32817747+rigabri@users.noreply.github.com>
2021-12-01 18:45:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
 
+#include "lib.h"
+
+bool isChar(char c) {
+    int a = int(c);
+    return ((65 <= a && a <= 90) || (97 <= a && a <= 122));
+}
+
+char convert(char c) {
+    int a = int(c);
+    if (a <= 90)
+        a += 32;
+    else
+        a-= 32;
+    return char(a);
+}
